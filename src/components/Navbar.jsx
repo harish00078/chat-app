60859bd35b1e292b3cdc56d66945e5c5ee8cb4b6
harish kumar <harishpathania150@gmail.com>
2023-09-21 20:11:47 +0530

    up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import SignIn from './SignIn'
-import LogOut from './LogOut'
-import {auth} from '../firebase'
-import {useAuthState} from 'react-firebase-hooks/auth'
+import React from "react";
+import SignIn from "./SignIn";
+import LogOut from "./LogOut";
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 const style = {
-    nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
-    heading: `text-white text-3xl`
-}
+  nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
+  heading: `text-white text-3xl`,
+};
 
 const Navbar = () => {
-    const [user] = useAuthState(auth)
-    console.log(user)
+  const [user] = useAuthState(auth);
+  console.log(user);
   return (
     <div className={style.nav}>
       <h1 className={style.heading}>Chat App</h1>
       {user ? <LogOut /> : <SignIn />}
-
     </div>
   );
 };
